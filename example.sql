76eda185d88916fda9ec9e9874d710cf185a6393
GreziaP <146154501+retteyam@users.noreply.github.com>
2025-04-27 00:52:33 -0500

    Add files via upload

---
+++ example.sql
@@ -0,0 +1,12 @@
+INSERT INTO producto(nombre, familia_id, descripcion, precio)
+VALUES
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado a lad faldskjfaf + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15),
+('Pescado Frito + Café + Yuca', 1, '(pescado frito, café, yuca hervida,zarza', 15);
+
+select * from familia;
