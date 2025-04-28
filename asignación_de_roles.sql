9e948f6c3e941bc4f7c64baf4ecd6c0bd6184669
GreziaP <146154501+retteyam@users.noreply.github.com>
2025-04-27 20:43:33 -0500

    Create asignación_de_roles.sql

---
+++ asignación_de_roles.sql
@@ -0,0 +1,12 @@
+-- Asignar el rol 'admin' al usuario 'admin_user'
+GRANT 'admin' TO 'admin_user'@'143.14.157.173';
+SET DEFAULT ROLE 'admin' TO 'admin_user'@'143.14.157.173';
+-- Asignar el rol 'empleado' al usuario 'empleado_venta1'
+GRANT 'empleado' TO 'empleado_venta1'@'143.14.157.173';
+SET DEFAULT ROLE 'empleado' TO 'empleado_venta1'@'143.14.157.173';
+-- Asignar el rol 'empleado' al usuario 'empleado_venta2'
+GRANT 'empleado' TO 'empleado_venta2'@'143.14.157.173';
+SET DEFAULT ROLE 'empleado' TO 'empleado_venta2'@'143.14.157.173';
+-- Asignar el rol 'cajero' al usuario 'cajero_user'
+GRANT 'cajero' TO 'cajero_user'@'143.14.157.173';
+SET DEFAULT ROLE 'cajero' TO 'cajero_user'@'143.14.157.173';
